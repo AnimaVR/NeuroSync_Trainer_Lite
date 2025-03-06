a8cc885150f5a2feb208a84f25acc7baa3cc39d5
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-06 00:16:25 +0000

    Update training_helpers.py

--- utils/training_helpers.py
+++ utils/training_helpers.py
@@ -6,7 +6,7 @@
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
-
+from torch.cuda.amp import GradScaler, autocast
 
 def init_weights(m):
     if isinstance(m, (nn.Linear, nn.Conv1d)):
